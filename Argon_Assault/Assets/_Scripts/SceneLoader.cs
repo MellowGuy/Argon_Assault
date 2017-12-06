1fd94591c26e4330902dac6d2eb02ed662a75fce
Tyler R Thompson <thotyl88@gmail.com>
2017-12-05 21:47:07 -0500

    Made SceneLoader script

---
+++ Argon_Assault/Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	//Amount of time to wait in seconds
+	public float waitTime = 3f;
+
+	// Use this for initialization
+	void Start()
+	{
+		Invoke("LoadFirstScene", waitTime);
+	}
+
+	void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+		Debug.Log("Scene Loaded");
+	}
+}
